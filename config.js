0e385128deef2a528b1646ad0e161691dac99fc4
Amith <mkamithkumar@gmail.com>
2013-06-06 17:32:11 +0530

    hadoop elipse plugin gist added

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
 var config = {
-  title: 'Grite',
+  title: 'Hadoop Eclipse Plugin',
   navbar: [ { text: 'Fork me on Github',
-              link: 'https://github.com/sankha93/grite' },
-            { text: 'Documentation',
-              link: 'https://github.com/sankha93/grite/wiki/Documentation'} ],
-  posts: [ '5711471',
-           '5711492'],
+              link: 'https://github.com/mkamithkumar/hadoop-eclipse-plugins' }
+           ],
+  posts: [ '5670765'],
   atOnce: 5,
-  footerText: "Grite has been made with love by Sankha. Follow him on Twitter at <a href=\"https://twitter.com/sankha93\">@sankha93</a>."
+  footerText: "Follow this blog and create your own eclipse plugins"
 };
